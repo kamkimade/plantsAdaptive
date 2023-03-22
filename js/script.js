bbfe3821e07662953fba71e2267fc46eb8499e15
Adolf Kim <letsplayagain@inbox.ru>
2023-03-22 17:08:03 +0500

    Added script.js
    1. burger action added in script.js
    2. media.css modified
    3. added 'Burger selectors' - style.css

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// burger action
+const burgerBtn     = document.querySelector('.burger');
+const lineBtnTop    = document.querySelector('.burger-top');
+const lineBtnCenter = document.querySelector('.burger-center');
+const lineBtnBottom = document.querySelector('.burger-bottom');
+const burgerMenu    = document.querySelector('.burger-menu');
+const navLinks      = document.querySelectorAll('.nav__link');
+const navList       = [];  
+let delay = 0;
+
+for (let i=0; i<navLinks.length; i++) {
+  navList.push(document.createElement('a'));
+  navList[i].href = navLinks[i].href;
+  navList[i].classList.add('burger-menu__link');
+  navList[i].style.animationDelay = `${delay}ms`;
+  navList[i].textContent = navLinks[i].textContent;
+  delay+= 200;
+}
+
+burgerBtn.addEventListener('click', startBurger);
+
+function startBurger() {
+  burgerBtnToggle();
+  if (burgerMenu.className.includes('burger-menu--active')) {
+    burgerMenu.classList.remove('burger-menu--active');
+    removeChildrenElem(burgerMenu, navList);
+  } else {
+    navList.forEach((link) => {
+      link.addEventListener('click', () => {
+        burgerMenu.classList.remove('burger-menu--active');
+        removeChildrenElem(burgerMenu, navList)
+        burgerBtnToggle()
+      } )
+    })
+    burgerMenu.classList.add('burger-menu--active');
+    addChildrenElem(burgerMenu,navList); 
+  }
+}
+
+function addChildrenElem(obj,node) {
+  node.forEach((elem) => {
+    obj.appendChild(elem);
+  })
+
+}
+
+function removeChildrenElem(obj,node) {
+  node.forEach((elem) => {
+    obj.removeChild(elem);
+  })
+}
+
+function burgerBtnToggle() {
+  lineBtnCenter.classList.toggle('burger--active-center');
+  lineBtnTop.classList.toggle('burger--active-top');
+  lineBtnBottom.classList.toggle('burger--active-bottom');
+}
+
+function addElementlistener() {
+  for (let el of navList) {
+    console.log(el)
+    el.addEventListener('click', () => {
+      if (burgerMenu.className.includes('burger-menu--active')) {
+        burgerMenu.classList.remove('burger-menu--active');
+        removeChildrenElem(burgerMenu, navLinks)
+        burgerBtnToggle();
+      }
+    } )
+  }
+}
